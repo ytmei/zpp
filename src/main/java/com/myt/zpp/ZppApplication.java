2c88c6c25ac35800d3a055be444a7bb61359c79b
meiyt <1011707758@qq.com>
2021-12-17 10:45:04 +0800

    Initial commit

---
+++ src/main/java/com/myt/zpp/ZppApplication.java
@@ -0,0 +1,13 @@
+package com.myt.zpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myt/zpp/ZppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myt.zpp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
